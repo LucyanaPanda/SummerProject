af0ba0515f2ad89d1f0768c8b2e0966cce69aff3
LucyanaPanda <lucie.nguyenpham@gmail.com>
2025-07-28 18:29:06 +0200

    Finish
    whenever the player camera collide with an object that could blocked the view from the player to his character, the camera will now move to the hitpoint to still film the character

--- Assets/Scripts/Camera/LockCamera.cs
+++ Assets/Scripts/Camera/LockCamera.cs
@@ -15,16 +15,15 @@ public class LockCamera : MonoBehaviour
         Vector3 direction = cameraFollowScript.transform.position - playerTransform.position;
         Physics.Raycast(playerTransform.position, direction.normalized, out RaycastHit hit, maxRadius);
 
-        if (hit.collider != null)
+        if (hit.collider != null  && !hit.collider.CompareTag("Player"))
         {
             print(hit.collider.gameObject.name);
-            LockCamera script = hit.collider.GetComponent<LockCamera>();
-            if (script != null)
-            {
-                float distance = Vector3.Magnitude(hit.point - playerTransform.position);
-                distance = Mathf.Max(distance, maxRadius);
-                cameraFollowScript.Radius = distance;
-            }
+            float distance = Vector3.Magnitude(hit.point - playerTransform.position);
+            distance = Mathf.Min(distance, maxRadius);
+            cameraFollowScript.Radius = distance;
+            print(distance);
         }
+        else
+            cameraFollowScript.Radius = maxRadius;
     }
 }
