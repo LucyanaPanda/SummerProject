70acbe221ad1891921deea68c71ebf7fe5d459a0
Lucie <lucie.nguyenpham@gmail.com>
2026-01-06 10:39:06 +0100

    Push first changes

--- Assets/Scripts/Camera/LockCamera.cs
+++ Assets/Scripts/Camera/LockCamera.cs
@@ -8,13 +8,12 @@ public class LockCamera : MonoBehaviour
     
     [Header("Camera Settings")]
     [SerializeField] private CinemachineOrbitalFollow cameraFollowScript;
-    private float currentRadius = 5f;
+    [SerializeField] private float currentRadius = 5f;
     private bool isColliding = false;
 
     private void Update()
     {
         Vector3 direction = cameraFollowScript.transform.position - playerTransform.position;
-
         Physics.Raycast(playerTransform.position, direction.normalized, out RaycastHit hit, currentRadius);
 
         if (hit.collider != null  && !hit.collider.CompareTag("Player"))
@@ -26,13 +25,6 @@ public class LockCamera : MonoBehaviour
             }
 
             float distance = Vector3.Magnitude(hit.point - playerTransform.position);
-            if (distance <= 0.2f)
-            {
-                cameraFollowScript.transform.position = -transform.position;
-                cameraFollowScript.transform.localRotation = Quaternion.Euler(-transform.localRotation.x, -transform.localRotation.y, -transform.localRotation.z);
-                return;
-            }
-
             distance = Mathf.Min(distance, currentRadius);
             cameraFollowScript.Radius = distance;
         }
---
+++ Assets/Scripts/Interfaces/Lootable/LootableObject.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public interface LootableObject
+{
+    void Loot();
+}
